3911c5504e3e7adf78f28f5f31f323160e1004d9
Jader-T <jader.theisges@gmail.com>
2023-01-25 10:22:12 -0300

    Criada nova classe para cálculos, e agora resolve bhaskara

--- SIN.py
+++ SIN.py
@@ -1,11 +1,11 @@
 import discord
 from decouple import config
 from discord.ext import commands
-
+import math
 bot = commands.Bot("$")
 
 bot.load_extension("commands.manager")
 bot.load_extension("commands.talks")
-
+bot.load_extension("commands.calc")
 TOKEN = config("TOKEN")
 bot.run(TOKEN)
